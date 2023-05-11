9d2ab01b3711171d1339f87be0c1d0fcc0c86800
Alec Boutin <alec.boutin@amplitude.com>
2023-05-11 13:03:16 -0700

    ---

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { RouterProvider, createBrowserRouter, Link } from "react-router-dom";
+import Page1 from "./page1";
+import { useEffect } from "react";
+import { track, init } from "@amplitude/analytics-browser";
 
-function App() {
+function App1() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Link to="/page1">Page1</Link>
+
+      <button
+        onClick={() => {
+          track("sign_up", { plan: "free" });
+        }}
+      >
+        Sign up
+      </button>
+      <button
+        onClick={() => {
+          track("add_to_cart", { product_id: "1234" });
+        }}
+      >
+        Add to cart
+      </button>
     </div>
   );
+}
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App1 />,
+  },
+  {
+    path: "/page1",
+    element: <Page1 />,
+  },
+]);
+
+function App() {
+  useEffect(() => {
+    init("f6b112eb749ccce87a5a26ea5fe89fee", undefined, {
+      defaultTracking: {
+        pageViews: true,
+        sessions: true,
+        formInteractions: true,
+        fileDownloads: true,
+      },
+    });
+  }, []);
+
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/page1.js
@@ -0,0 +1,5 @@
+const Page1 = () => {
+    return <div>This is Page1</div>;
+}
+
+export default Page1;
